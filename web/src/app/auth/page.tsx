d0e9bd3cabfe63987a8838d69b4dedf5f18f836e
toddmurch <t.murchjr@gmail.com>
2025-05-05 15:29:01 -0500

    basic init for nextjs

---
+++ web/src/app/auth/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="main flex h-full">
+      <div className="container flex items-center text-center">
+        <h1>Auth</h1>
+      </div>
+    </main>
+  );
+}
