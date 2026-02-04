4723bfea5ccb470e29ddb80a78617fca94fca6df
toddmurch <t.murchjr@gmail.com>
2026-02-03 21:49:06 -0600

    Revert "fix(web): resolve build failure from NextError in App Router"
    This reverts commit 8f6f2f86921787ecd8e06af66c458ed12c4ce5ba.

--- web/src/app/global-error.tsx
+++ web/src/app/global-error.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import * as Sentry from '@sentry/nextjs';
+import NextError from 'next/error';
 import { useEffect } from 'react';
 
 export default function GlobalError({
@@ -11,11 +12,14 @@ export default function GlobalError({
   useEffect(() => {
     Sentry.captureException(error);
   }, [error]);
-
   return (
     <html>
       <body>
-        <h1>Something went wrong!</h1>
+        {/* `NextError` is the default Next.js error page component. Its type
+        definition requires a `statusCode` prop. However, since the App Router
+        does not expose status codes for errors, we simply pass 0 to render a
+        generic error message. */}
+        <NextError statusCode={0} />
       </body>
     </html>
   );
